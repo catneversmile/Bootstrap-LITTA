e48065f0dd409c6411dacf1e7c8c71dfc74b8a1b
catneversmile <chenweikuncsu@163.com>
2018-03-31 22:50:18 +0800

    -a

---
+++ js/index.js
@@ -0,0 +1,44 @@
+$(function(){
+	//滚动时顶边栏变色
+
+
+	$(window).scroll(function(){
+		if(screen.availWidth>768){
+			if(window.pageYOffset>100){
+				$(".top-nav").css({
+					"padding-top":"0px",
+					"padding-bottom":"0px",
+					"backgroundColor":'rgba(0,0,0,.85)',				
+				});
+			}else{
+				$(".top-nav").css({
+					"padding-top":"20px",
+					"padding-bottom":"20px",
+					"backgroundColor":'rgba(0,0,0,0)',				
+				});	
+			}
+		}
+	})
+
+	//容器元素背景满屏宽
+	fullWidth(".tank-margin",".tank");
+	$(window).resize(function(){
+		fullWidth(".tank-margin",".tank");
+	});
+
+
+
+})
+
+
+
+// container子无素背景宽为屏宽100%,
+function fullWidth(value1,value2){
+		var paddingValue = parseInt($(value1).css("margin-left"))+15+"px";
+		$(value2).css({
+			"margin-left" : ("-"+paddingValue),
+			"margin-right": ("-"+paddingValue),
+			"padding-left": paddingValue,
+			"padding-right": paddingValue,
+		}); 		
+	}
